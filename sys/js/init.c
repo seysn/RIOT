ae09d375c3b555fabcafbf8b1986bfc57c14b971
Nicolas SEYS <seys.nicolas@gmail.com>
2019-02-11 09:24:36 +0100

    Removed http module

--- sys/js/init.c
+++ sys/js/init.c
@@ -13,9 +13,6 @@ extern const unsigned saul_methods_len;
 extern const js_native_method_t coap_methods[];
 extern const unsigned coap_methods_len;
 
-extern const js_native_method_t http_methods[];
-extern const unsigned http_methods_len;
-
 void js_init_objects(void)
 {
     jerry_value_t global_object = jerry_get_global_object();
@@ -35,11 +32,6 @@ void js_init_objects(void)
     jerry_value_t coap_object = js_object_create_with_methods(coap_methods, coap_methods_len);
     js_add_object(global_object, coap_object, "coap");
     jerry_release_value(coap_object);
-
-    jerry_value_t http_object = js_object_create_with_methods(http_methods, http_methods_len);
-    js_add_object(global_object, http_object, "http");
-    jerry_release_value(http_object);
-
 
     jerry_release_value(global_object);
 
